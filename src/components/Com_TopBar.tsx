d3e40c529feae4f4a2bc78caf74eb08d85b54dd9
杨再涛 <1772081518@qq.com>
2026-02-03 00:00:51 +0800

    封装头部关闭小化操作按钮

--- src/components/Com_TopBar.tsx
+++ src/components/Com_TopBar.tsx
@@ -1,11 +1,20 @@
 import { Icon } from "@iconify/react";
 import logo from "@/assets/icon/icon.svg";
 import { useNavigation } from "react-router-dom";
+import { window } from "@tauri-apps/api";
+import { useEffect, useState } from "react";
 const app_name = import.meta.env['VITE_APPNAME'] as string;
+
+const win = window.getCurrentWindow();
 export default function () {
     const navigate = useNavigation();
-    return <div className="theme_0 px-2 py-1 flex justify-between shadow-sm">
-        <div className="flex">
+    const [isfull, setIsFull] = useState(false);
+
+    useEffect(() => {
+        win.isMaximized().then(setIsFull)
+    }, [])
+    return <div className="theme_0 px-2 py-1 flex justify-between shadow-sm drag">
+        <div className="flex no_drag">
             <div className=" inline-flex">
                 <img src={logo} />
                 <span className=" ml-2 font-bold">{app_name}</span>
@@ -14,10 +23,10 @@ export default function () {
                 {JSON.stringify(navigate.location)}
             </div>
         </div>
-        <div className="inline-flex select-none">
-            <Icon icon="line-md:minus-square-twotone" width="24" className="cursor-pointer active:scale-95" />
-            <Icon icon="line-md:uploading-loop" width="24" className="cursor-pointer active:scale-95" />
-            <Icon icon="line-md:close-circle-twotone" width="24" className="cursor-pointer active:scale-95 text-red-500" />
+        <div className="inline-flex select-none no_drag">
+            <Icon onClick={async () => await win.minimize()} icon="line-md:minus-square-twotone" width="24" className="cursor-pointer active:scale-95" />
+            <Icon onClick={async () => { await win.toggleMaximize(), setIsFull(!isfull) }} icon={isfull ? 'line-md:downloading-loop' : 'line-md:uploading-loop'} width="24" className="cursor-pointer active:scale-95" />
+            <Icon onClick={async () => await win.close()} icon="line-md:close-circle-twotone" width="24" className="cursor-pointer active:scale-95 text-red-500" />
         </div>
     </div>
 }
